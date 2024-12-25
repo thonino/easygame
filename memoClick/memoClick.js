f0e10f4fb437722be598fccaa9af1721aa4c79f9
thonino <thoninoben@gmail.com>
2024-12-25 02:41:31 +0100

    good progress

--- memoClick/memoClick.js
+++ memoClick/memoClick.js
@@ -1,14 +1,15 @@
 
-//  Create tab
 let row1 = document.getElementById("row1");
 let row2 = document.getElementById("row2");
 let row3= document.getElementById("row3");
 let row4 = document.getElementById("row4");
 let row5 = document.getElementById("row5");
 
+// Create tab
 let tabs = [];
 for(i = 1; i <= 30 ; i++){ tabs.push(i) }
 
+// Create random tab
 let tabRandom = [];
 while( tabs.length > 0){
   let numberRandom = tabs[Math.floor(Math.random()*tabs.length)]; 
@@ -16,63 +17,57 @@ while( tabs.length > 0){
   tabRandom.push(numberRandom); 
 }
 
+// show Tab
+let rows = [row1, row2, row3, row4, row5];
 tabRandom.forEach((item, index) => {
+  let rowIndex = Math.floor(index / 6); 
+  rows[rowIndex].innerHTML += `
+    <button  
+      id="it-${item}" 
+      value="${item}"
+      class="${item <= 10 ? 'btn-info' : 'btn-secondary'} btn fs-1 fw-bold col-2"
 
-  if( index < 6) {
-    row1.innerHTML += `
-    <button  
-      id="tabRandom" 
-      class="${item <= 10 ? 'btn-info' : 'btn-secondary'} btn fs-1 fw-bold col-2"
     >
       ${item} 
     </button>
-    `
-  }
-  else if( index >= 6 && index < 12 ) {
-    row2.innerHTML += `
-    <button  
-      id="tabRandom" 
-      class="${item <= 10 ? 'btn-info' : 'btn-secondary'} btn fs-1 fw-bold col-2"
-    >
-      ${item} 
-    </button>
-    `
-  }
-  else if( index >= 12 && index < 18 ) {
-    row3.innerHTML += `
-    <button  
-      id="tabRandom" 
-      class="${item <= 10 ? 'btn-info' : 'btn-secondary'} btn fs-1 fw-bold col-2"
-    >
-      ${item} 
-    </button>
-    `
-  }
-  else if( index >= 18 && index < 24 ) {
-    row4.innerHTML += `
-    <button  
-      id="tabRandom" 
-      class="${item <= 10 ? 'btn-info' : 'btn-secondary'} btn fs-1 fw-bold col-2"
-    >
-      ${item} 
-    </button>
-    `
-  }
-  if ( index >= 24 ){
-    row5.innerHTML += `
-    <button  
-      id="tabRandom" 
-      class="${item <= 10 ? 'btn-info' : 'btn-secondary'} btn fs-1 fw-bold col-2"
-    >
-      ${item} 
-    </button>
-    `
-  }
+  `;
 });
 
-
-
-
+// HandleGame
+let tabResult = [];
+for(i = 1; i <= 10 ; i++){ tabResult.push(i)}
+let countClick = 1 ;
+// listen button 
+tabRandom.forEach(item => {
+  let listenBtn = document.getElementById("it-"+item);
+  let progressbar = document.getElementById("progressbar");
+  let message = document.getElementById("message");
+  let message2 = document.getElementById("message2");
+  listenBtn.addEventListener("click", () => {
+    if(countClick == listenBtn.value){
+      if(countClick == 10){ 
+        message.innerHTML =`<p id="message" class="text-success"> Perfect You Win ! </p>`
+      } else {
+        message.innerHTML =`<p id="message" class="text-info">${listenBtn.value} Yes !</p>`
+      }
+      message2.innerHTML =`<p id="message2"></p>`
+      progressbar.style.width = (countClick*10)+"%";
+      countClick ++;
+    } else {
+      message.innerHTML =`<p id="message" class="text-danger"> You lose !</p>`
+      message2.innerHTML =`<p id="message2" class="text-info h2"> Try again ? </p>`
+      countClick = 1;
+      progressbar.style.width = "0%";
+    }
+  })
+});
+// resetGame
+function newgame() { 
+  message.innerHTML =`<p id="message"></p>`
+  message2.innerHTML =`<p id="message2"></p>`
+  countClick = 1;
+  progressbar.style.width = "0%";
+}
 
 
 
